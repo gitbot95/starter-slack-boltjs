058b4d4f46307b1106852f8a7bb5d7ed83e416d1
Dang Minh Thinh <d-thinh@ruby-dev.vn>
2022-08-08 17:56:16 +0700

    Toggle view

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { App, ExpressReceiver } = require("@slack/bolt");
-const getLessonList = require("./firebase");
+const viewModal = require("./firebase");
 
 const receiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -17,140 +17,13 @@ const app = new App({
 app.command("/learn", async ({ ack, body, client, logger }) => {
   // Acknowledge the command request
   await ack();
-  lists = await getLessonList();
-
   try {
-    console.log(lists);
     // Call views.open with the built-in client
     const result = await client.views.open({
       // Pass a valid trigger_id within 3 seconds of receiving it
       trigger_id: body.trigger_id,
       // View payload
-      view: {
-        type: "modal",
-        submit: {
-          type: "plain_text",
-          text: "Let's Go!",
-          emoji: true,
-        },
-        close: {
-          type: "plain_text",
-          text: ":thinking_face:",
-          emoji: true,
-        },
-        title: {
-          type: "plain_text",
-          text: "Learn new words",
-          emoji: true,
-        },
-        blocks: [
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "Lessons list",
-            },
-            accessory: {
-              type: "multi_static_select",
-              placeholder: {
-                type: "plain_text",
-                text: "Select options",
-                emoji: true,
-              },
-              options: [
-                {
-                  text: { type: "plain_text", text: "Lesson 1", emoji: true },
-                  value: "lesson_1",
-                },
-                {
-                  text: { type: "plain_text", text: "Lesson 2", emoji: true },
-                  value: "lesson_2",
-                },
-                {
-                  text: { type: "plain_text", text: "Lesson 3", emoji: true },
-                  value: "lesson_3",
-                },
-              ],
-              action_id: "multi_static_select-action",
-            },
-          },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "Everyday learn",
-            },
-            accessory: {
-              type: "static_select",
-              placeholder: {
-                type: "plain_text",
-                text: "Select an item",
-                emoji: true,
-              },
-              options: [
-                {
-                  text: {
-                    type: "plain_text",
-                    text: "5 words",
-                    emoji: true,
-                  },
-                  value: "5",
-                },
-                {
-                  text: {
-                    type: "plain_text",
-                    text: "10 words",
-                    emoji: true,
-                  },
-                  value: "10",
-                },
-                {
-                  text: {
-                    type: "plain_text",
-                    text: "15 words",
-                    emoji: true,
-                  },
-                  value: "15",
-                },
-              ],
-              action_id: "static_select-action",
-            },
-          },
-          {
-            type: "section",
-            text: {
-              type: "plain_text",
-              text: "Select date range",
-              emoji: true,
-            },
-          },
-          {
-            type: "actions",
-            elements: [
-              {
-                type: "datepicker",
-                initial_date: "1990-04-28",
-                placeholder: {
-                  type: "plain_text",
-                  text: "Select a date",
-                  emoji: true,
-                },
-                action_id: "actionId-0",
-              },
-              {
-                type: "datepicker",
-                initial_date: "1990-04-28",
-                placeholder: {
-                  type: "plain_text",
-                  text: "Select a date",
-                  emoji: true,
-                },
-                action_id: "actionId-1",
-              },
-            ],
-          },
-        ],
-      },
+      view: viewModal,
     });
     logger.info("GOTCHA!");
   } catch (error) {
--- firebase.js
+++ firebase.js
@@ -56,4 +56,121 @@ getLessonList = async () => {
   return list;
 };
 
-module.exports = getLessonList;
+viewModal = async () => {
+  const list = await getLessonList();
+  viewGenerated = {
+    type: "modal",
+    submit: {
+      type: "plain_text",
+      text: "Let's Go!",
+      emoji: true,
+    },
+    close: {
+      type: "plain_text",
+      text: ":thinking_face:",
+      emoji: true,
+    },
+    title: {
+      type: "plain_text",
+      text: "Learn new words",
+      emoji: true,
+    },
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Lessons list",
+        },
+        accessory: {
+          type: "multi_static_select",
+          placeholder: {
+            type: "plain_text",
+            text: "Select options",
+            emoji: true,
+          },
+          options: list,
+          action_id: "multi_static_select-action",
+        },
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: "Everyday learn",
+        },
+        accessory: {
+          type: "static_select",
+          placeholder: {
+            type: "plain_text",
+            text: "Select an item",
+            emoji: true,
+          },
+          options: [
+            {
+              text: {
+                type: "plain_text",
+                text: "5 words",
+                emoji: true,
+              },
+              value: "5",
+            },
+            {
+              text: {
+                type: "plain_text",
+                text: "10 words",
+                emoji: true,
+              },
+              value: "10",
+            },
+            {
+              text: {
+                type: "plain_text",
+                text: "15 words",
+                emoji: true,
+              },
+              value: "15",
+            },
+          ],
+          action_id: "static_select-action",
+        },
+      },
+      {
+        type: "section",
+        text: {
+          type: "plain_text",
+          text: "Select date range",
+          emoji: true,
+        },
+      },
+      {
+        type: "actions",
+        elements: [
+          {
+            type: "datepicker",
+            initial_date: "1990-04-28",
+            placeholder: {
+              type: "plain_text",
+              text: "Select a date",
+              emoji: true,
+            },
+            action_id: "actionId-0",
+          },
+          {
+            type: "datepicker",
+            initial_date: "1990-04-28",
+            placeholder: {
+              type: "plain_text",
+              text: "Select a date",
+              emoji: true,
+            },
+            action_id: "actionId-1",
+          },
+        ],
+      },
+    ],
+  };
+  return viewGenerated;
+};
+
+module.exports = viewModal;
