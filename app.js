25fbe4adf58226364ea05a135c98c6ca5b95752c
Dang Minh Thinh <d-thinh@ruby-dev.vn>
2022-08-16 10:51:25 +0700

    Test action

--- app.js
+++ app.js
@@ -23,7 +23,52 @@ app.command("/learn", async ({ ack, body, client, logger }) => {
       // Pass a valid trigger_id within 3 seconds of receiving it
       trigger_id: body.trigger_id,
       // View payload
-      view: content,
+      view: {
+        type: "modal",
+        submit: {
+          type: "plain_text",
+          text: "Let's Go!",
+          emoji: true,
+        },
+        close: {
+          type: "plain_text",
+          text: ":thinking_face:",
+          emoji: true,
+        },
+        title: {
+          type: "plain_text",
+          text: "Learn new words",
+          emoji: true,
+        },
+        blocks: [
+          {
+            type: "section",
+            block_id: "b",
+            text: {
+              type: "mrkdwn",
+              text: "Pick a customer from the dropdown list",
+            },
+            accessory: {
+              type: "multi_static_select",
+              action_id: "lessons-action",
+              placeholder: {
+                type: "plain_text",
+                text: "Select a customer",
+              },
+              min_query_length: 0,
+              options: [
+                {
+                  text: {
+                    type: "plain_text",
+                    text: "option 1",
+                  },
+                  value: "value-1",
+                },
+              ],
+            },
+          },
+        ],
+      },
     });
     logger.info(`${"=".repeat(20)}GOTCHA!${"=".repeat(20)}`);
   } catch (error) {
